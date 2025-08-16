1d6dba3fe5da78781aafaf5e7dc3e6af237184eb
Marcel Nunez <marcelpnunez@gmail.com>
2025-08-15 20:09:55 -0700

    Add and subtract for cost

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ fn main() {
 trait Amount {
     fn get_amount(&self, color: &str) -> i32;
     fn can_be_afforded_by(&self, cost: &impl Amount) -> bool;
+    fn add(&self, other: &Self) -> Self;
+    fn sub(&self, other: &Self) -> Self;
 }
 
 impl Amount for HashMap<String, i32> {
@@ -28,20 +30,36 @@ impl Amount for HashMap<String, i32> {
         }
         return true;
     }
+    fn add(&self, other: &Self) -> Self {
+        let mut result = self.clone();
+        for (color, value) in other.iter() {
+            *result.entry(color.clone()).or_insert(0) += value;
+        }
+        result
+    }
+
+    fn sub(&self, other: &Self) -> Self {
+        let mut result = self.clone();
+        for (color, value) in other.iter() {
+            *result.entry(color.clone()).or_insert(0) -= value;
+        }
+        result
+    }
 }
 
-struct Card {
+struct Card<T: Amount> {
     points: i32,
-    //cost: impl Amount,
+    cost: T,
+    color: String,
 }
 
-trait Deck {
-    fn draw_card(&mut self) -> Option<Card>;
+trait Deck<T: Amount> {
+    fn draw_card(&mut self) -> Option<Card<T>>;
     fn shuffle(&mut self, rng: &mut ThreadRng);
 }
 
-impl Deck for Vec<Card> {
-    fn draw_card(&mut self) -> Option<Card> {
+impl<T: Amount> Deck<T> for Vec<Card<T>> {
+    fn draw_card(&mut self) -> Option<Card<T>> {
         return self.pop();
     }
 
@@ -50,12 +68,12 @@ impl Deck for Vec<Card> {
     }
 }
 
-struct CardTier {
-    pub cards_for_purchase: Vec<Card>,
+struct CardTier<T: Amount> {
+    pub cards_for_purchase: Vec<Card<T>>,
 }
 
 trait Player {
-    fn buy_card(&self, card: &Card) -> Result<(), String> {
+    fn buy_card<T: Amount>(&self, card: &Card<T>) -> Result<(), String> {
         // if card.cost.is_affordable(self) {
         //     // Logic to buy the card
         //     Ok(())
@@ -89,37 +107,64 @@ impl GameState {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::{rstest,fixture};
 
+    #[fixture]
+    pub fn cost() -> HashMap<String, i32> {
+        let mut c: HashMap<String, i32> = HashMap::new();
+        c.insert(String::from("red"), 1);
+        c.insert(String::from("blue"), 2);
+        c.insert(String::from("green"), 3);
+        c
+    }
+
+    #[fixture]
+    pub fn bank() -> HashMap<String, i32> {
+        let mut b: HashMap<String, i32> = HashMap::new();
+        b.insert(String::from("red"), 1);
+        b.insert(String::from("blue"), 1);
+        b.insert(String::from("green"), 1);
+        b
+    }
+
+    #[fixture]
+    pub fn bank2() -> HashMap<String, i32> {
+        let mut b: HashMap<String, i32> = HashMap::new();
+        b.insert(String::from("red"), 4);
+        b.insert(String::from("blue"), 4);
+        b.insert(String::from("green"), 4);
+        b
+    }
+
+    #[rstest]
     #[test]
-    fn generate_possible_moves() {
-        let mut rng = thread_rng();
-
-        let mut cost: HashMap<String, i32> = HashMap::new();
-
-        // Insert key-value pairs
-        cost.insert(String::from("red"), 1);
-        cost.insert(String::from("blue"), 2);
-        cost.insert(String::from("green"), 3);
-
+    fn can_afford(
+        cost: HashMap<String, i32>,
+        bank: HashMap<String, i32>,
+        bank2: HashMap<String, i32>
+    ) {
         assert_eq!(cost.get_amount("red"), 1);
         assert_eq!(cost.get_amount("black"), 0);
-
-        let mut bank: HashMap<String, i32> = HashMap::new();
-
-        // Insert key-value pairs
-        bank.insert(String::from("red"), 1);
-        bank.insert(String::from("blue"), 1);
-        bank.insert(String::from("green"), 1);
-
         assert!(!cost.can_be_afforded_by(&bank));
+        assert!(cost.can_be_afforded_by(&bank2));
+    }
 
-        let mut bank2: HashMap<String, i32> = HashMap::new();
+    #[rstest]
+    #[test]
+    fn add_and_sub(
+        cost: HashMap<String, i32>,
+        bank: HashMap<String, i32>,
+        bank2: HashMap<String, i32>
+    ) {
+        let result = cost.add(&bank);
+        assert_eq!(result.get_amount("red"), 2);
+        assert_eq!(result.get_amount("blue"), 3);
+        assert_eq!(result.get_amount("green"), 4);
+        assert_eq!(result.get_amount("black"), 0);
 
-        // Insert key-value pairs
-        bank2.insert(String::from("red"), 4);
-        bank2.insert(String::from("blue"), 4);
-        bank2.insert(String::from("green"), 4);
-
-        assert!(bank2.can_be_afforded_by(&bank2));
+        let result2 = bank2.sub(&cost);
+        assert_eq!(result2.get_amount("red"), 3);
+        assert_eq!(result2.get_amount("blue"), 2);
+        assert_eq!(result2.get_amount("green"), 1);
     }
 }
